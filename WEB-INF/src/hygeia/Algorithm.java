2c0a5e26112fb171b7e2835277935bf577994e55
Alexander Morris <admorris@ucsd.edu>
2012-02-23 12:40:52 -0800

    Moved encryption algorithm into Algorithm class

--- WEB-INF/src/hygeia/Algorithm.java
+++ WEB-INF/src/hygeia/Algorithm.java
@@ -1,6 +1,7 @@
 package hygeia;
 
 import java.sql.*;
+import java.security.*;
 
 public class Algorithm {
 
@@ -14,6 +15,22 @@ public class Algorithm {
     /* Main algorithm. types: 1 breakfast, 2 lunch, 3 dinner, 4 snack */
     public Meal suggestMeal(User u, int type) {
     
+    }
+    
+    /* Should return MD5 hashes.. but may be platform dependent. And this was 
+       written by some anonymous author. FYI.  */
+    public static String MD5(String md5) {
+        try {
+            java.security.MessageDigest md = java.security.MessageDigest.getInstance("MD5");
+            byte[] array = md.digest(md5.getBytes());
+            StringBuffer sb = new StringBuffer();
+            for (int i = 0; i < array.length; ++i) {
+                sb.append(Integer.toHexString((array[i] & 0xFF) | 0x100).substring(1,3));
+            }
+            return sb.toString();
+        } catch (java.security.NoSuchAlgorithmException e) {
+        }
+        return null;
     }
 
 }
