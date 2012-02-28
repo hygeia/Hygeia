fe12de3c5b592a24945a10111bfd19c458c00ccb
Alexander Morris <admorris@ucsd.edu>
2012-02-28 13:02:51 -0800

    Wrote Database Simple Tutorial

--- dumps/hygeia3.sql
+++ dumps/hygeia3.sql
@@ -3,7 +3,7 @@ grant all privileges on hygeia3.* to 'hygeiadb'@'localhost';
 use hygeia3;
 create table users (uid integer primary key auto_increment, username text, hpwd text, email text, height real, weight real);
 create table inventory (iid integer primary key auto_increment, uid integer, fid integer, count real);
-create table foods (fid integer primary key auto_increment, uid integer, name text, weight integer, factor real, calories real, carbohydrates real, protein real, fat real);
+create table foods (fid integer primary key auto_increment, uid integer, name text, weight real, factor real, calories real, carbohydrates real, protein real, fat real);
 create table components (cid integer primary key auto_increment, mid integer, fid integer, count real);
 create table history (hid integer primary key auto_increment, mid integer, uid integer, occurence timestamp);
 create table favorites (fid integer primary key auto_increment, mid integer, uid integer);
