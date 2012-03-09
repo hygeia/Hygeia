72adc30ceffb96ee16fcde6e05a506d340efa6d8
Corbin Lewis <CorbinLLewis@gmail.com>
2012-03-08 20:38:25 -0800

    This is corbin's first commit. Pop.

--- WEB-INF/src/hygeia/Algorithm.java
+++ WEB-INF/src/hygeia/Algorithm.java
@@ -2,24 +2,94 @@ package hygeia;
 
 import java.sql.*;
 import java.security.*;
+import java.util.StringTokenizer;
+import java.util.ArrayList;
 
 public class Algorithm {
 
     private Database db;
     private int uid;
+    private final double SAME = 0.1; //used as a margin of error for relatively balanced meals
 
     public Algorithm(Database db, User u) {
-    
+		this.uid = u.uid;
+		this.db = u.db;
     }
 
     /* Main algorithm. types: 1 breakfast, 2 lunch, 3 dinner, 4 snack */
     public Meal suggestMeal(User u, int type) {
+		//pulls all meals from the universal meal list and the user's personal meals
+        ResultSet rs = db.execute("select mid from meals where uid = " + u.uid + " or uid = 0;");
+        //arraylist of meal IDs that come from the database
+        ArrayList<int> results = new ArrayList();
+		while(rs.next())
+		{
+			results.add(rs.getInt("mid"));
+		}
+		//retrieves a list of food in the inventory
+		Food.Update[] fu = Inventory.getInventory();
+		//random generator to select a meal at random from available MIDs
+		Random r = new Random(results.size());
+		//if the inventorymatchcount variable equals the number of ingredients in a recipe, all necessary ingredients are available
+		int inventorymatchcount = 0;
+		//Meal m is the variable used to store meals as they are accessed for comparison to ingredients
+		Meal m = new Meal(db, 0);
+		//while loop runs while a suitable meal isn't found yet
+		while (results.size() > 0)
+		{
+			inventorymatchcount = 0;
+			m = new Meal(db, results.get(r.nextInt()));
+			for (int i = 0; i < m.getMeal().getCount(); i++)
+			{
+				for (int j = 0; j < fu.getCount(); j++)
+				{
+					if (m.getMeal()[i] == fu[j])
+						inventorymatchcount += 1;
+				}
+			}
+			if (inventorymatchcount == m.getCount())
+			{
+				//currently not calorie budget based. Functionality will be added if budget is accessible.
+				//begins balanced suggestion based on the 40:30:30 ideal,
+				//+ and - 10% (defined as constant SAME, Suggest A Meal Error) to find relatively balanced meals
+				Nutrition n = new Nutrition(m.getNutrition()); //makes a nutrition object holding the meal's total nutrition
+				double totalGrams = n.getCarbohydrates() + n.getProtein() + n.getFat();
+				if (n.getCarbohydrates() / totalGrams > 0.4 - SAME 
+						&& n.getCarbohydrates() / totalGrams < 0.4 + SAME)
+				{
+					if (n.getProtein() / totalGrams > 0.3 - SAME 
+							&& n.getProtein() / totalGrams < 0.3 + SAME)
+					{
+						if (n.getFat() / totalGrams > 0.3 - SAME 
+								&& n.getFat() / totalGrams < 0.3 + SAME)
+						{
+							return m;
+						}
+					}
+				}
+			}
+			else
+			{
+				//if the contents of the inventory don't satisfy the recipe, remove that recipe
+				//from the ArrayList of meals so it won't accidentally be compared again
+				results.remove(m.getMid());
+				r = new Random(results.size());
+			}
+
+		}
+		//if no meal matches the SAME margin of error for balancedness, return null
         return null;
     }
     
     /* Sanitizes a String for use. */
     public static String Clean(String s) {
-        return s;
+    	StringTokenizer toke = new StringTokenizer(s, "*/\\\"\':;-()=+[]");
+    	String r = new String("");
+    	while(toke.hasMoreTokens())
+    	{
+    		r = new String(r + toke.nextToken());
+    	}
+        return r;
     }
     
     /* Should return MD5 hashes.. but may be platform dependent. And this was 
@@ -37,5 +107,4 @@ public class Algorithm {
         }
         return null;
     }
-
 }
--- WEB-INF/src/hygeia/History.java
+++ WEB-INF/src/hygeia/History.java
@@ -1,5 +1,5 @@
 package hygeia;
-
+import java.util.ArrayList;
 import java.sql.*;
 
 public class History {
@@ -9,28 +9,68 @@ public class History {
     
     /* Creates user's history object */
     public History(User u) {
-    
+    	this.db = u.db;
+    	this.uid = u.uid;
     }
     
     /* Returns an array of Meal objects */
     public Meal.List[] getHistory() {
-    
+    	ResultSet rs1 = db.execute("select * from history where uid = " + this.uid + " order by occurrence desc;");
+    	ResultSet rs2;
+    	ArrayList<Meal.List> mla = new ArrayList();
+    	Meal.List temp = new Meal.List("a", 0, null);
+    	String tempName = new String("");
+    	int tempMid = 0;
+    	Timestamp tempTimestamp = new TimeStamp();
+    	ArrayList<int> mlaseed = new ArrayList();
+    	while(rs1.next())
+    	{
+			tempMid = rs1.getInt(0);
+			rs2 = db.execute("select * from meals where mid = " + tempMid + ";");
+			rs2.next();
+			temp = new Meal.List(rs2.getString("name"), rs2.getInt("mid"), rs1.getString("occurrence"));
+			mla.add(temp);
+    	}
+    	
+    	mla.add(temp);
+    	return mla.toArray();
     }
     
     /* Returns an array of meals that are available to the user to add to
        the history or favorites. */
-    public Meal.List[] getAvailableMeals() {
-    
+    public Meal.List[] getAvailableMeals(String s) {
+		s = new String(Clean(s));
+		ResultSet rs = db.execute("select * from meals where (uid = 0 or uid = " + uid + ") and name like \"%" + s + "%\";");
+    	ArrayList<Meal.List> mla = new ArrayList();
+    	Meal.List temp = new Meal.List("a", 0, null);
+    	String tempName = new String("");
+    	int tempMid = 0;
+    	Timestamp tempTimestamp = new TimeStamp();
+    	while (rs.next())
+    	{
+			tempName = rs.getString("name");
+			tempMid = rs.getInt("mid");
+			tempTimestamp = rs.getString("occurrence");
+			temp = new Meal.List(tempName, tempMid, tempTimestamp);
+			mla.add(temp);
+		}
+		return mla.toArray();
     }
     
     /* Add meal to history */
     public boolean addMeal(Meal m, Timestamp occurrence) {
     
+		db.update("insert into history (mid, uid, occurence) values (" 
+			+ m.getMid() + "," + uid + "," + occurrence + ");");
+		return true;
     }
     
     /* Remove meal from history. */
     public boolean removeMeal(Meal m, Timestamp occurrence) {
     
+		db.update("delete from history where mid = " + m.getMid() 
+			+ " and uid = " + uid + " and occurence = \"" + occurrence + "\";");
+		return true;
     }
 
 }
