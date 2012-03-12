2b5f1498a53e734c1ba9a8ce2d6746357a566f72
Alexander Morris <admorris@ucsd.edu>
2012-03-12 12:42:04 -0700

    Updated Favorites Class

--- WEB-INF/src/hygeia/Favorites.java
+++ WEB-INF/src/hygeia/Favorites.java
@@ -15,44 +15,56 @@ public class Favorites {
     
     /* Add a meal to the favorites */
     public boolean addMeal(Meal m) {
-		//this needs to be fixed to auto-increment the fid field
-		db.update("insert into favorites values (" + m.getMid() + ", " + uid + ";");
+        if (m == null) {
+            return false;
+        }
+		int r = db.update("insert into favorites (mid, uid) values (" + 
+		    m.getMid() + ", " + uid + ";");
+		if (r < 1) {
+		    return false;
+		}
 		return true;
     }
     
     /* Remove a meal from favorites */
     public boolean removeMeal(Meal m) {
-		ResultSet rs = db.execute("select fid from favorites where uid = " + uid + " and mid = " + m.getMid() + ";");
-		if (rs == null)
-			return false;
-		else
-		{
-			db.update("delete from favorites where uid = " + uid + " and mid = " + m.getMid() + ";");
-			return true;
+		
+		if (m == null) {
+		    return false;
 		}
+		
+		int r = db.update("delete from favorites where uid = " + uid + 
+		    " and mid = " + m.getMid() + ";");
+		if (r < 1) {
+		    return false;
+		}
+		return true;
 	}
     
     /* Returns an array of Meal objects in favorites */
     public Meal.List[] getFavorites() {
-		ArrayList<Meal.List> ml = new ArrayList();
-		ArrayList<Integer> il = new ArrayList();
-		ResultSet rs1 = db.execute("select mid from favorites where uid = " + uid + ";");
-		while(rs1.next())
-		{
-			il.add(rs1.getInt(mid));
-		}
-		String s = new String("");
-		for (int i = 0; i < il.size() - 1; i++)
-		{
-			s += il.get(i) + ", ";
-		}
-		s += il.get(il.size() - 1);
-		ResultSet rs2 = db.execute("select * from meals where mid = " + s + ";");
-		while(rs2.next())
-		{
-			ml.add(new Meal.List(rs2.getString(name), rs2.getInt(mid), "");
-		}
-		return ml.toArray();
+    
+        ArrayList<Meal.List> ml = new ArrayList<Meal.List>();
+        
+        ResultSet rs = this.db.execute("select favorites.mid, meals.name from" +
+            " favorites inner join meals on favorites.mid=meals.mid where " +
+            "favorites.uid = " + this.uid + ";");
+        
+        try {
+            if (rs == null) {
+                return null;
+            }
+            
+            while (rs.next()) {
+                String name = rs.getString("name");
+                int mid = rs.getInt("mid");
+                ml.add(new Meal.List(name, mid, null));
+            }
+            db.free();
+            return (Meal.List[])ml.toArray(new Meal.List[1]);
+        } catch (SQLException e) {
+            return null;
+        }
     }
     
     /* Returns an array of meals that are available to the user to add to
